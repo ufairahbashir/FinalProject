f9d4b9d63a1cc7f7b7180d17efe2cbc78b8afe10
ufairahbashir <ufairahbashir@gmail.com>
2022-06-01 11:53:54 +0800

    FinalProject App

---
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private LinearLayout linearLayout;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        linearLayout = (LinearLayout) findViewById(R.id.homecreen);
+        linearLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivity2();
+            }
+        });
+    }
+
+    public void openActivity2(){
+        Intent intent = new Intent(this, home.class);
+        startActivity(intent);
+    }
+}
