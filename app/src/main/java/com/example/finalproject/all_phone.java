11d03f31ce8be96cd1ea0f4c43a895dd7636a0e9
ufairahbashir <ufairahbashir@gmail.com>
2022-06-02 11:50:18 +0800

    FinalProject App

---
+++ app/src/main/java/com/example/finalproject/all_phone.java
@@ -0,0 +1,14 @@
+package com.example.finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class all_phone extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_phone);
+    }
+}
