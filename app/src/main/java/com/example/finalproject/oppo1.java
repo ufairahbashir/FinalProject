c25ae4753741a38e48a025d89996e90a7c6c3fbe
ufairahbashir <ufairahbashir@gmail.com>
2022-06-04 17:39:51 +0800

    FinalProject App

--- app/src/main/java/com/example/finalproject/oppo1.java
+++ app/src/main/java/com/example/finalproject/oppo1.java
@@ -2,13 +2,37 @@ package com.example.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class oppo1 extends AppCompatActivity {
+
+    public
+    EditText nama_user; EditText komentar;
+    Button btnReview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_oppo1);
+
+        nama_user = findViewById(R.id.username);
+        nama_user.setOnClickListener(new View.OnClickListener() {
+
+        });
+
+        komentar = findViewById(R.id.review);
+        komentar.setOnClickListener(new View.OnClickListener() {
+
+        });
+
+        btnReview = findViewById(R.id.btnreview);
+        btnReview.setOnClickListener(new View.OnClickListener() {
+
+        });
     }
 }
---
+++ app/src/test/java/review.java
@@ -0,0 +1,26 @@
+public class review {
+
+    public String namaUser;
+    public String komentar;
+
+    public review(String namaUser, String komentar) {
+        this.namaUser = namaUser;
+        this.komentar = komentar;
+    }
+
+    public String getNamaUser() {
+        return namaUser;
+    }
+
+    public void setNamaUser(String namaUser) {
+        this.namaUser = namaUser;
+    }
+
+    public String getKomentar() {
+        return komentar;
+    }
+
+    public void setKomentar(String komentar) {
+        this.komentar = komentar;
+    }
+}
